e0d92d3cebc4aa2fc0a8f8f7bf0c68b0013396bc
jack-aaass <3314696867@qq.com>
2025-05-01 21:52:10 +0800

    pull test

--- myweb/src/main/java/com/powernode/servlets/WelcomeServlet.java
+++ myweb/src/main/java/com/powernode/servlets/WelcomeServlet.java
@@ -30,6 +30,7 @@ public class WelcomeServlet extends HttpServlet {
         System.out.println("dev1");
         System.out.println("master1");
         System.out.println("dev2");
+        System.out.println("pull test");
         out.print("<h3>welcome to study servlet!</h3>");
         Connection conn = null;
         PreparedStatement ps = null;
